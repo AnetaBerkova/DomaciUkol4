c15e93167b6603aacb171b66e2a843d22818064e
Aneta Berková <aneta.berkova11@gmail.com>
2024-03-10 21:44:12 +0100

    Domaci ukol 4

---
+++ script.js
@@ -0,0 +1,11 @@
+const max3 = (a, b, c) => {
+    if (a >= b && a >= c) {
+        return a;
+    } else if (b >= a && b >= c) {
+        return b;
+    } else {
+        return c;
+    }
+}
+
+console.log(max3(15, 0, 11))
